d9e8ea2a97ae84490a2c6967476f0362e2433d12
aditya thakor <aditya.github.27x@gmail.com>
2026-04-11 16:39:49 +0530

    create admin .

--- Decibullz_Backend/modules/admin/createAdmin.js
+++ Decibullz_Backend/modules/admin/createAdmin.js
@@ -1,12 +1,17 @@
+import dotenv from 'dotenv';
 import mongoose from "mongoose";
-import { Admin } from "./models/admin.model";
+import { Admin } from "./models/admin.model.js";
+import connectDB from '../../config/db.js';
+import path from 'path';
+import { envpath } from '../../envpath.js';
 
-mongoose.connect(process.env.MONGODB_URL);
+dotenv.config({ path: path.resolve(envpath+'.env') });
 
 (async ()=>{
+    await connectDB();
     await Admin.create({
-        email:process.env.ADMIN_EMAIL,
-        password:process.env.ADMIN_PASS
+        email:String(process.env.ADMIN_EMAIL),
+        password:String(process.env.ADMIN_PASS)
     });
 
     console.log("Admin created");
